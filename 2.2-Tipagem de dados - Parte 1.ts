bc2faf8922d0e5912b830a4aa9388aed9f620b18
Eriton Santos <eritonsg@gmail.com>
2018-11-23 10:50:58 -0300

    Tipagem de dados - Parte 1

---
+++ 2.2-Tipagem de dados - Parte 1.ts
@@ -0,0 +1,20 @@
+/*
+    Booleanos: boolean
+    Pontos flutuantes: number (podem ser representados em octal, hexadecimal, binário)
+    Strings: string
+*/ 
+let meuBooleano: boolean = false;
+let numero: number = 1.1;
+let numero2: number = 4;
+let meuTexto: string = 'Olá TreinaWeb.';
+
+let logradouro: string = "Avenida Paulista";
+let num: string = "123B";
+let cidade: string = "São Paulo"
+let estado: string = "SP";
+
+// String patterns
+alert(`Endereço: ${logradouro}, ${num}, ${cidade}-${estado}`);
+
+let meuArray: number[] = [1, 2, 3];
+let meuArray2: Array<number> = [1, 2, 3];
